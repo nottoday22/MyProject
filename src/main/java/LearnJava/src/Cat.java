ace63147de1ff02e75e88704a83828b6d299c6e1
Vladimir <n0tt0day@mail.ru>
2021-01-03 13:12:13 +0300

    Add new code

--- src/main/java/LearnJava/src/Cat.java
+++ src/main/java/LearnJava/src/Cat.java
@@ -1,36 +1,47 @@
 package LearnJava.src;
 
-public class Cat
-{
+public class Cat {
+    public static int count = 0;
     private double originWeight;
     private double weight;
-
     private double minWeight;
     private double maxWeight;
+    private double eatenFood;
 
-    public Cat()
-    {
+
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
-
+        count++;
     }
 
-    public void meow()
-    {
+    public void meow() {
         weight = weight - 1;
         System.out.println("Meow");
     }
 
-    public void feed(Double amount)
-    {
+    public void pee() {
+        weight = weight - 10;
+        System.out.println("Pee on sofa");
+    }
+
+    public void feed(Double amount) {
+        eatenFood = amount + eatenFood;
         weight = weight + amount;
     }
 
-    public void drink(Double amount)
-    {
+    public void drink(Double amount) {
         weight = weight + amount;
+    }
+
+    public Double getSumOfFood(){
+        return eatenFood;
+    }
+
+    public static int getCount(){
+        return count;
     }
 
     public Double getWeight()
@@ -41,9 +52,11 @@ public class Cat
     public String getStatus()
     {
         if(weight < minWeight) {
+            count--;
             return "Dead";
         }
         else if(weight > maxWeight) {
+            count--;
             return "Exploded";
         }
         else if(weight > originWeight) {
--- src/main/java/LearnJava/src/Loader.java
+++ src/main/java/LearnJava/src/Loader.java
@@ -1,28 +1,42 @@
 package LearnJava.src;
 
+import java.io.IOException;
+
 public class Loader
 {
-    public static void main(String[] args)
-    {
-        Cat cat = new Cat();
+    public static void main(String[] args) throws IOException {
         Cat vaska = new Cat();
-        Cat tima = new Cat();
-        System.out.println(vaska.getWeight());
-        System.out.println(tima.getWeight());
-        System.out.println("Cat is " + cat.getStatus());
+        Cat murka = new Cat();
+        Cat kot = new Cat();
+        Cat dron = new Cat();
+        Cat hrom = new Cat();
 
-//        vaska.feed(3000.42);
-//        System.out.println("Vaska is " + vaska.getStatus());
-//
-//        tima.drink(6900.23);
-//        System.out.println("Tima is " + tima.getStatus());
+        System.out.println("Вес Васьки: " + vaska.getWeight());
+        System.out.println("Вес Мурки: " +murka.getWeight());
+        System.out.println("Вес Кота: " +kot.getWeight());
+        System.out.println("Вес Дрона: " +dron.getWeight());
+        System.out.println("Вес Хрома: " +hrom.getWeight());
 
-        while (true){
-            vaska.meow();
-            if(vaska.getWeight() < 1000){
-                System.out.println(vaska.getStatus());
+        vaska.feed(50550.53);
+        kot.feed(53.32);
+        System.out.println("Теперь вес Васьки стал: " + vaska.getWeight());
+        System.out.println("Теперь вес Кота стал: " + kot.getWeight());
+        System.out.println("Васька должен взорваться: " + vaska.getStatus());
+
+        while (true) {
+            kot.meow();
+            if(kot.getWeight() < 1000) {
+                System.out.println("Кот замяукан до смерти? : " + kot.getStatus());
                 break;
             }
         }
+
+        hrom.feed(150.00);
+        hrom.feed(150.00);
+        hrom.pee();
+        hrom.pee();
+        System.out.println("Сколько хром съел грамм всего? : " + hrom.getSumOfFood());
+
+        System.out.println("Сколько котиков всего: " + Cat.getCount());
     }
 }
